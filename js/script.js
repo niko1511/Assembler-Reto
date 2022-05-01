fc0479552522c19e8ee17ff1fcda69f3a75d0482
niko1511 <niko1511@gmail.com>
2022-05-01 13:47:51 +0200

    add all file

---
+++ js/script.js
@@ -0,0 +1,32 @@
+// event document addEventListener
+document.getElementById("tab").addEventListener("click", tabMove);
+
+var tab = document.getElementById("tab");
+var cont = document.getElementById("content");
+var point = document.getElementById("point");
+
+  text = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates, reiciendis ex expedita illo, sint nulla. Nisi quis dignissimos est rerum, amet vitae molestiae incidunt, dolorum excepturi. Magni, optio quas maiores!";
+
+
+tab.classList.toggle("slider-tab-off");
+cont.classList.toggle("content-Max-Width");
+//cont.classList.toggle("containerControl");
+point.classList.toggle("navigation-manual-reduction");
+
+// event touch move tab
+function tabMove() {
+  tab.classList.toggle("slider-tab-off");
+  cont.classList.toggle("content-Max-Width");
+  //cont.classList.toggle("containerControl");
+  point.classList.toggle("navigation-manual-reduction");
+}
+
+//funtion for navigation automatic
+var counter = 1;
+setInterval(function () {
+  document.getElementById("radio" + counter).checked = true;
+  counter++;
+  if (counter > 4) {
+    counter = 1;
+  }
+},5000);
